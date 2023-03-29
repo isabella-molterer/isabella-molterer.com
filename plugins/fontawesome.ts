ed15962f9cc7e9a75d8483db782d46e7e4791944
isabella-molterer <isabella.molterer@gmail.com>
2023-03-29 12:12:16 +0200

    refactor: remove unused fontawesome icon

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -8,13 +8,13 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 /* import specific icons */
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
-import { faPaperPlane, faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
 /* add each imported icon to the library */
-library.add(faGithub, faLinkedinIn, faPaperPlane, faMoon);
+library.add(faGithub, faLinkedinIn, faPaperPlane);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("FontAwesomeIcon", FontAwesomeIcon);
